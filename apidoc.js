e2cd9f4f0b67f38390898c6b09685c9996f413a3
Xandor Xicay <xandorxicay@Admins-MacBook-Pro-5.local>
2014-10-23 12:14:48 -0400

    apidocs init

---
+++ apidoc.js
@@ -0,0 +1,13 @@
+/**
+*@api{get}/user Request User Information
+*@apiName Getuser
+*@apiGroup User
+*@apiVersion 0.1.0
+*
+*@apiSuccess{String} name The users name.
+*
+*@apiSuccessExample Example data on success: 
+*{
+*  name: 'Zach'
+*}
+*/
