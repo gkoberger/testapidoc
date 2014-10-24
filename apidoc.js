99932c9127dbccce7746f056505eb2d2f46fdf36
Gregory Koberger <gkoberger@gmail.com>
2014-10-24 14:36:35 -0700

    Spacing

--- apidoc.js
+++ apidoc.js
@@ -1,10 +1,10 @@
 /**
-*@api{get}/user Request User Information
+*@api {get} /user Request User Information
 *@apiName Getuser
 *@apiGroup User
 *@apiVersion 0.1.0
 *
-*@apiSuccess{String} name The users name.
+*@apiSuccess {String} name The users name.
 *
 *@apiSuccessExample Example data on success: 
 *{
